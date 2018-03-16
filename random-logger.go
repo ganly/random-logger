af093e1124bbab2793804eb62546036c835aea98
Steve Ganly <steve.ganly@gmail.com>
2018-03-16 17:26:59 +0000

    Added rough and ready initial implementation which suits our initial needs

---
+++ random-logger.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	for c := 0; c < 8; c++ {
+
+		fmt.Println(randomString(10))
+		time.Sleep(2000 * time.Millisecond)
+		
+	}
+}
+
+func randomString(l int) string {
+	bytes := make([]byte, l)
+	for i := 0; i < l; i++ {
+		bytes[i] = byte(randInt(65, 90))
+	}
+	return string(bytes)
+}
+
+func randInt(min int, max int) int {
+	return min + rand.Intn(max - min)
+}
+
